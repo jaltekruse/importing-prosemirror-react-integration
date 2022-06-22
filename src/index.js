30451da960490cd6e2c2a6984a9b9813fe77c957
Jason Altekruse <altekrusejason@gmail.com>
2022-06-22 08:48:35 -0500

    WIP - Downgraded React version to match depedency, still getting hooks error
    - I can render a component with a hook defined in this project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Editor } from '@pm-react-example/full-v2';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// here is a basic functional component using hooks
+// from here: https://reactjs.org/docs/hooks-overview.html
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+console.log(Editor);
+const root = ReactDOM.render(
+    <Example />,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
